d8d82778d7966c01bc795365c230d250ab7ef9c3
Sashitha <user@DESKTOP-6H9VGAS>
2024-03-24 12:56:34 +0530

    develop add cart

--- src/app/layout/post-card/post-card.component.ts
+++ src/app/layout/post-card/post-card.component.ts
@@ -4,6 +4,9 @@ import { RouterModule } from '@angular/router';
 import { ApiService } from '../../shared/api.service';
 import { postcard } from './productmodal';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AddCardComponent } from '../../add-card/add-card.component';
+
 
 @Component({
   selector: 'app-post-card',
@@ -18,12 +21,16 @@ import { HttpClientModule } from '@angular/common/http';
 export class PostCardComponent implements OnInit{
   data: postcard[] = []; 
 
-  constructor(private api:ApiService){}
+  constructor(private api:ApiService,private modalService: NgbModal){}
   ngOnInit(): void {
     this.displayproducts();
   }
 displayproducts(){
   // console.log("dta fetching starting")
   this.data = this.api.getProduct()
+}
+
+handleOpenAddCard() {
+  const modalRef = this.modalService.open(AddCardComponent);
 }
 }
