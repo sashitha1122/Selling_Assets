7245c85eeb8dd90d56e8f3d538b6f02940decbb9
Sashitha <user@DESKTOP-6H9VGAS>
2024-07-04 09:00:08 +0530

    fetch the url

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -2,86 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { postcard } from '../layout/post-card/productmodal';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  apiurl = "https://localhost:7095/api/Assets";
+
   constructor(private http:HttpClient) { }
 
-  dataDummy: postcard[] = [
-    {
-      "id":"1",
-      "name": "Macbook1",
-      "url" : "../../assets/lepmac1.jpg" ,
-      "model": "Asus Rog Strix",
-      "update": "LAST-UPDATED - FEB 20,2024"
-    },
-    {
-      "id":"2",
-      "name" : "Macbook",
-      "url"  : "../../assets/lepmac.jpg",
-      "model": "model-2",
-      "update": "LAST-UPDATED - JAn 18,2024"
-    },
-    {
-      "id":"3",
-      "name" : "Macbook1",
-      "url"  : "../../assets/lepmac1.jpg",
-      "model": "model-2",
-      "update": "LAST-UPDATED - MAR 28,2024"
-    },
-    {
-      "id":"4",
-      "name" : "Macbook",
-      "url"  : "../../assets/lepmac.jpg",
-      "model": "Asus Rog Strix",
-      "update": "LAST-UPDATED - FEB 12,2024"
-    }
-    ,
-    {
-      "id":"5",
-      "name" : "Macbook1",
-      "url"  : "../../assets/lepmac.jpg",
-      "model": "model-2",
-      "update": "LAST-UPDATED - MAR 28,2024"
-    },
-    {
-      "id":"6",
-      "name" : "Asus Rog Strix R7-6800H",
-      "url"  : "../../assets/Asus Rog.webp",
-      "model": "Asus Rog Strix",
-      "update": "LAST-UPDATED - FEB 15,2024"
-    },
-    {
-      "id":"7",
-      "name": "Asus Rog Strix R7-6800H",
-      "url" : "../../assets/lepmac1.jpg" ,
-      "model": "Asus Rog Strix",
-      "update": "LAST-UPDATED - FEB 20,2024"
-    },
-    {
-      "id":"8",
-      "name" : "Macbook",
-      "url"  : "../../assets/Asus Rog.webp",
-      "model": "model-2",
-      "update": "LAST-UPDATED - JAn 18,2024"
-    },
-   ];
-
-  getProduct(){
-    // console.log("Starting basic function")
-    const data = this.dataDummy;
-    // console.log("data fetched from basic", data)
-    return data;
+  getProduct(): Observable<any[]>{
+  
+    return this.http.get<any[]>(this.apiurl + "");
+   
   }
   addCart(){
-    const data = this.dataDummy;
-    this.dataDummy.push();
+    
   }
-  getProductbyid(id: string): Observable<postcard | undefined> {
-    const product = this.dataDummy.find(item => item.id === id);
-    return of(product);
+  getProductbyid(id: string): Observable<any[]> {
+    return this.getProduct().pipe(
+      map((products: any[]) => products.find(item => item.id === id))
+    );
   }
 }
--- src/app/viewmore/viewmore.component.ts
+++ src/app/viewmore/viewmore.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute } from '@angular/router';
 import { postcard } from '../layout/post-card/productmodal';
 import { CartService } from '../shared/cart.service';
 import { CommonModule } from '@angular/common';
+import { error } from 'console';
 
 
 @Component({
@@ -16,25 +17,25 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./viewmore.component.scss'] 
 })
 export class ViewmoreComponent implements OnInit{
-  @Input() productId: string | undefined;
-  productData:undefined | postcard;
-
+  @Input() productId!: string ;
+   product: any;
   constructor(private api:ApiService,private activatedRoute:ActivatedRoute,private modalService: NgbModal,private cartService :CartService){
   }
 
   ngOnInit(): void {
     if (this.productId) {
       this.api.getProductbyid(this.productId).subscribe(
-        (result: postcard | undefined) => {
+        (result: any) => {
           console.log('Product data:', result); // Check fetched product data
-          this.productData = result;
+          this.product = result;
         },
-        error => {
+        (error: any) => {
           console.error('Error fetching product:', error); // Handle error fetching product
         }
       );
     }
   }
+
   addtoCart(item : any){
     this.cartService.addtoCart(item);
   }
